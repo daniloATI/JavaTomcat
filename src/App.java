ce1464f3bb2287ec9488bc52b48e7d7b05b6b585
Danilo CS <danilo.souza@ati.to.gov.br>
2025-09-10 23:23:54 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá Mundo!!!");
+        System.out.println("Hello, World!");
+        System.out.println("Olá Mundo!");
     }
 }
+
 
